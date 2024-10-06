d4f7ad4b2b1f038e43fd2e0da1a60f50a6c2eac8
Snehashish Srivastava <114405059+Snehashish-s@users.noreply.github.com>
2024-10-05 22:45:08 -0400

    final submission

--- data-processing.py
+++ data-processing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 df=pd.read_csv("data_sample.csv")
-df['ActualCost']=round(df['RawMaterial']+df['Workmanship']+df['StorageCost'],2)
-df['SoldPrice']=round(df['EstimatedCost']*1.1,2)
-df['MarginOfProfit']=round(df['SoldPrice']-df['ActualCost'],2)
+df['ActualCost']=df['RawMaterial']+df['Workmanship']+df['StorageCost']
+df['SoldPrice']=df['EstimatedCost']*1.1
+df['MarginOfProfit']=df['SoldPrice']-df['ActualCost']
 df.to_csv("data_sample_modified.csv")
